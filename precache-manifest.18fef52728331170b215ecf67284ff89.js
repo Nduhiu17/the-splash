c9d04c3f6419f6a1bba97d6983f1128757c99cd0
Nduhiu17 <nduhiu254@gmail.com>
2020-08-14 16:51:48 +0300

    Updates

---
+++ precache-manifest.18fef52728331170b215ecf67284ff89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018d778ee3a71667576a87439a357f73",
+    "url": "/the-splash/index.html"
+  },
+  {
+    "revision": "7e0815be64770d3d6157",
+    "url": "/the-splash/static/css/main.bde35c56.chunk.css"
+  },
+  {
+    "revision": "22f958b0ca8f8b0a78fe",
+    "url": "/the-splash/static/js/2.f076d1b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-splash/static/js/2.f076d1b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e0815be64770d3d6157",
+    "url": "/the-splash/static/js/main.10ef9d3e.chunk.js"
+  },
+  {
+    "revision": "6e4982386e0328e80916",
+    "url": "/the-splash/static/js/runtime-main.3f0e687d.js"
+  }
+]);
